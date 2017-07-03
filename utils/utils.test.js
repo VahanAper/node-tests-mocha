311e09ea8fa028112bd533a7ce36b0984625afd3
VahanAper <asryanvahandev@gmail.com>
2017-07-03 13:01:56 +0400

    make assertion with expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,36 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   const res = utils.add(33, 11);
-
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   const res = utils.square(2);
-
-  if (res !== 4) {
-    throw new Error(`Expected 4, but got ${res}`);
-  }
+  expect(res).toBe(4).toBeA('number');
 });
+
+it('should expect some values', () => {
+  expect(12).toNotBe(11);
+  expect({ name: 'Vahan' }).toEqual({ name: 'Vahan' });
+  expect([2, 3, 4]).toInclude(2);
+  expect([2, 3, 4]).toExclude(5);
+  expect({
+    name: 'Vahan',
+    age: 30,
+    location:
+    'Yerevan'
+  }).toInclude({ age: 30});
+});
+
+it('should verify first and last names are set', () => {
+  const user = { age: 30, location: 'Yerevan' };
+  const newUser = utils.setName(user, 'Vahan Asryan');
+
+  expect(newUser)
+  .toBeA('object')
+  .toInclude({ firstName: 'Vahan' })
+  .toInclude({ lastName: 'Asryan' });
+})
